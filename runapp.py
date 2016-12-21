bfd969648e44832d4a67d5855f28e87220e6aa88
Rick Valenzuela <devnull@rickv.com>
2016-12-20 19:02:51 -0800

    fixed 'server' to 'serve'

--- runapp.py
+++ runapp.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:production.ini', relative_to='.')
 
-    server(app, host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port)
 

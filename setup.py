86d763059d5e61ff8a491cf197f3e4f75311b819
Rick Valenzuela <devnull@rickv.com>
2016-12-20 17:24:30 -0800

    redid setup.py

--- setup.py
+++ setup.py
@@ -13,14 +13,15 @@ requires = [
     'pyramid_jinja2',
     'pyramid_debugtoolbar',
     'waitress',
-    'ipython'
-    'pyramid_ipython'
+    'ipython',
+    'pyramid_ipython',
     ]
 
 tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
     'pytest',  # includes virtualenv
     'pytest-cov',
+    'tox',
     ]
 
 setup(name='learning_journal_basic',

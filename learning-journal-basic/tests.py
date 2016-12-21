0421191f83575982d79ee28dbce6e6d1a8465b6d
Rick Valenzuela <devnull@rickv.com>
2016-12-20 16:58:52 -0800

    redoing folder structure

---
+++ learning-journal-basic/tests.py
@@ -0,0 +1,15 @@
+import pytest
+from pyramid import testing
+
+@pytest.fixture
+def req():
+    the_request = testing.DummyRequest()
+    return the_request
+
+
+def test_home_page_renders_file_data(req):
+    """My homepage view returns data.""""
+    from .views import home_page
+    response = home_page(req)
+    assert
+

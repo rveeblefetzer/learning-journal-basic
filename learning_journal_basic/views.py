c77465da2a186ca77190f358ea2eafe3323e0649
Rick Valenzuela <devnull@rickv.com>
2016-12-21 19:59:46 -0800

    changed views, del template html files for *.jinja2 files

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -5,40 +5,35 @@ import os
 
 THIS_DIR = os.path.dirname(__file__)
 
-def home_page(request):
-    """View for the homepage."""
-    file_path = os.path.join(THIS_DIR, 'templates', 'index.html')
-    file_data = io.open(file_path).read()
-    return Response(file_data)
+
+ENTRIES = {
+    1: {"id": 1, "title": "Today, Dec. 16, I learned.", "creation_date": "12/17/2016", "body": "Sample text"},
+    2: {"id": 2, "title": "Today, Dec. 17, I learned.", "creation_date": "12/18/2016", "body": "Sample text"},
+    3: {"id": 3, "title": "Today, Dec. 18, I learned.", "creation_date": "12/19/2016", "body": "Sample text"},
+    4: {"id": 4, "title": "Today, Dec. 19, I learned.", "creation_date": "12/20/2016", "body": "Sample text"},
+    5: {"id": 5, "title": "Today, Dec. 20, I learned.", "creation_date": "12/21/2016", "body": "Sample text"},
+}
 
 
+@view_config(route_name="index", renderer="templates/index.jinja2")
+def register(request):
+    """View the list page, displaying a list of journal entries."""
+    return {"ENTRIES": ENTRIES}
+
+
+@view_config(route_name="detail", renderer="templates/entry.jinja2")
 def detail(request):
-    file_path = os.path.join(THIS_DIR, 'templates', 'entry.html')
-    file_data = io.open(file_path).read()
-    return Response(file_data)
+    """View an entry's page, displaying a certain entry."""
+    return {"ENTRIES": ENTRIES[int(request.matchdict['id'])]}
 
 
+@view_config(route_name="form", renderer="templates/write.jinja2")
 def create(request):
-    file_path = os.path.join(THIS_DIR, 'templates', 'write.html')
-    file_data = io.open(file_path).read()
-    return Response(file_data)
+    """View the form page, displaying a form to write new blog posts."""
+    return {"ENTRIES": ENTRIES}
 
 
-def edit(request):
-    file_path = os.path.join(THIS_DIR, 'templates', 'editentry.html')
-    file_data = io.open(file_path).read()
-    return Response(file_data)
-
-def includeme(config):
-    """"""
-    config.add_view(home_page,
-        route_name='home')
-
-    config.add_view(detail,
-        route_name='detail')
-
-    config.add_view(create,
-        route_name='form')
-
-    config.add_view(edit,
-        route_name='edit_form')
+@view_config(route_name="edit_form", renderer="templates/editentry.jinja2")
+def update(request):
+    """View the update/edit-form page, displaying a form to update/edit blog posts."""
+    return {"ENTRIES": ENTRIES[int(request.matchdict['id'])]}

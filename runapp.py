93c3f4a1c2951e6f7fb1fa279543dfe7703337da
Rick Valenzuela <devnull@rickv.com>
2016-12-27 21:08:59 -0800

    fixed typo in waitress' serve (from 'server')

--- runapp.py
+++ runapp.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:production.ini', relative_to='.')
 
-    server(app, host='127.0.0.1', port=port)
+    serve(app, host='127.0.0.1', port=port)
